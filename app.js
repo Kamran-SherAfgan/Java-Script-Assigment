ac2ffa0e40ddb98221e66e2ee7ca92f0477becce
Kamran Sher Afgan <64993556+Kamran-SherAfgan@users.noreply.github.com>
2020-06-14 16:33:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,307 @@
+// Chapter # 1 Task 1
+
+
+alert("Error! Please enter a valid password");
+alert("Welcome to JS land... happy coding")
+
+// Chapter # 1 Task 2
+
+alert("Welcome to JS land...\nhappy coding")
+
+// Chapter # 1 Task 3
+
+alert("Welcome to JS land...");
+alert("Happy coding!" , "Prevent this page from creating additional dialogues");
+
+// Chapter # 1 Task 4
+
+console.log("Hello...I can run JS through my web browser's console");
+
+////////////////////////////////
+
+// Chapter # 2 Task 1
+
+var username
+
+var myName = "Kamran Sher Afgan"
+
+// Chapter # 2 Task 2
+
+var messsage = "Hello World"
+alert(messsage);
+
+// Chapter # 2 Task 3
+
+var n = "Jhone Doe"
+var s = "15  years old"
+var k = "Certified Mobile Application Development"
+
+alert(n);
+alert(s);
+alert(k);
+
+// Chapter # 2 Task 4
+
+var n = "PIZZA";
+alert("PIZZA\nPIZZ\nPIZZ\nPIZ\nPI\nP");
+
+// Chapter # 2 Task 5
+
+var email = " kami.sher.ks@gmail.com";
+alert('My email address is' + '' + email);
+
+
+// Chapter # 2 Task 6
+
+var book = ("A smarter way to learn JavaScript");
+alert("I  am  trying to  learn from  the Book" + " " + book);
+
+// Chapter # 2 Task 7
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+//////////////////////////////////////
+
+
+
+// Chapter # 3 Task 1
+
+var age = 25
+alert("I am " + age + " years old");
+
+
+// Chapter # 3 Task 2
+
+var n = 14;
+alert("You have visited this site " + n + " times");
+
+// Chapter # 3 Task 3
+
+var birthYear = 1995;
+document.write("My birth year is " + birthYear + "<br/>");
+document.write("Data type of my declared variable is number");
+
+// Chapter # 3 Task 4
+
+var storeName = "www.XYZclothing.com";
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+
+document.write(visitorName + " ordered " + quantity + productTitle + " on XYZ Clothing store.");
+
+///////////////////////////////////
+
+
+// Chapter # 4 All tasks
+
+document.write("Rules for naming JS variables" + "<br/> <br/> <br/> <br/>" );
+document.write("Variable names can only contain , numbers, $ and _ . For example : $my_1stVariable" + "<br>");
+document.write("Variable must begin with a letter, $ or _ . For example : $name, _name or name" + "<br>")
+document.write("Variable names are case sensitive" + "<Br>");
+document.write("Variable names should not be JS keywords");
+
+// Chapter # 5 Task 1
+
+var n = 3;
+var k = 5;
+var s = (n+k);
+document.write("Sum of " + 3 + " and " + 5 + " is " + s + "<br> <br>");
+
+
+var n = 5;
+var s = 3;
+var k = (n-s);
+document.write("Subtraction of " + 5 + " and " + 3 + " is " + k + "<br><br>");
+
+
+var n = 3;
+var s = 5;
+var k = (n*s);
+document.write("Multiplication of " + 3 + " and " + 5 + " is " + k + "<br><br>");
+
+
+var n = 3;
+var s = 5;
+var k = (n/s);
+document.write("Division of " + 3 + " and " + 5 + " is " + k + "<br><br>");
+
+var n = 3;
+var s = 5;
+var k = (n%s);
+document.write("Modulus of " + 3 + " and " + 5 + " is " + k);
+
+// Chapter # 5 Task 2
+
+document.write("Value after variable declaration is undefined" + "<br>");
+document.write("Initial value: "  + 5 + "<br>");
+document.write("Value after increment is: " + 6 + "<br>");
+document.write("Value after addition is: " + 13 + "<br>");
+document.write("Value after decrement is: " + 12 + "<br>");
+document.write("The reamainder is: " + 0);
+
+
+// Chapter # 5 Task 3
+
+var costTicket = 600;
+var Tickets = 5;
+var totalCost = (costTicket*Tickets);
+document.write("Total cost to buy " + 5 + " tickests to a movie is " + totalCost + "PKR");
+
+// Chapter # 5 Task 4
+
+document.write("Table 0f "+ 4 + "<br>");
+document.write(4 + " x " + 1 + " = " + 4 + "<br>");
+document.write(4 + " x " + 2 + " = " + 8 + "<br>");
+document.write(4 + " x " + 3 + " = " + 12 + "<br>");
+document.write(4 + " x " + 4 + " = " + 16 + "<br>");
+document.write(4 + " x " + 5 + " = " + 20 + "<br>");
+document.write(4 + " x " + 6 + " = " + 24 + "<br>");
+document.write(4 + " x " + 7 + " = " + 28 + "<br>");
+document.write(4 + " x " + 8 + " = " + 32 + "<br>");
+document.write(4 + " x " + 9 + " = " + 36 + "<br>");
+document.write(4 + " x " + 10 + " = " + 40);
+
+// Chapter # 5 Task 5
+
+document.write(25 + "oC is " + 77 + "oF" + "<br>" );
+document.write(70 + "oF is " + 21.111111111111111 + "oC");
+
+// Chapter # 5 Task 6
+
+var price1 = 650;
+var price2 = 100;
+
+var quantity1 = 3;
+var quantity2 = 7;
+
+var charges = 100;
+
+document.write("Price of item " + 1 + " is " + price1 + "<BR>");
+document.write("Quantity of item " + 1 + " is " + quantity1 + "<br>");
+document.write("Price of item " + 2 + " is " + price2 + "<br>");
+document.write("Quantity of item " + 2 + " is " + quantity2 + "<br>" );
+
+document.write("Shipping charges " + charges + "<br>" + "<br>");
+
+document.write("Total cost of your order is " + (price1*3 + price2*7));
+
+// Chapter # 5 Task 8
+
+var totalMarks = 980;
+var marksObtained = 804;
+var percentage = (marksObtained/totalMarks*100);
+
+document.write("Total marks: " + totalMarks +"<br>");
+document.write("Marks obtained: " + marksObtained  + "<br>");
+document.write("Percentage: " + percentage);
+
+
+// Chapter # 5 Task 9
+
+var usDollar = 104.80;
+var saudiRiyal = 28;
+
+var totalDollars = 10;
+var totalSaudiriyals = 25;
+
+document.write("Total Currency in PKR: " + (usDollar*totalDollars + saudiRiyal*totalSaudiriyals));
+
+// Chapter # 5 Task 10
+
+var number = 5
+
+document.write("Arithmatic: " + (number+5*10/2));
+
+// Chapter # 5 Task 11
+
+var currentYear = 2016;
+var birthYear = 1992;
+var age = (currentYear-birthYear); 
+
+document.write("Current Year: " + currentYear + "<br>")
+document.write("Birth Year: " + birthYear + "<br>")  
+document.write("Your age is: " + age);
+
+
+// Chapter # 5 Task 12
+
+document.write("Radius of a circle is: " + 20 + "<br>");
+document.write("The circumference is: " + 125.679999999999 + "<br>")
+document.write("The area is: " + 1256.8);
+
+// Chapter # 5 Task 13
+
+var currentAge = 15;
+var maximumAge = 65;
+var snackDay = 3;
+
+var estimate = (maximumAge-currentAge);
+var estimatedSnacks = (estimate*3);
+
+document.write("Favorite Snack: chocolate chip" + "<br>");
+document.write("Current age: " + currentAge + "<br>");
+document.write("Estimated Maximum Age: " + maximumAge + "<br>");
+document.write("Amount of snacks per day; " +snackDay + "<br>");
+document.write("You will need " + estimatedSnacks + " chocolate chip to last you until the ripeold age of 65");
+
+// Chapter # 6-9 Task 1
+
+var a = 10
+
+document.write("Result:" + "<br>");
+document.write("The value of a is: " + a + "<br>") ;
+document.write("..................................." + "<br>" + "<br>");
+
+document.write("The value of ++a is: " + (++a) + "<br>");
+document.write("Now the value of a is: " + (a) + "<br>" + "<br>") ;
+
+
+document.write("The value of a++ is: " + (a++) + "<br>");
+document.write("Now the value of a is: " + (a) + "<br>" + "<br>");
+
+
+document.write("The value of --a is: " + (--a) + "<br>");
+document.write("Now the value of a is: " + (a) + "<br>" + "<br>");
+
+document.write("The value of a-- is: " + (a--) + "<br>");
+document.write("Now the value of a is: " + (a) + "<br>" + "<br>");
+
+
+// Chapter # 6-9 Task 2
+
+var a = 2;
+var b = 1;
+var result = (--a - --b + ++b + b--);
+
+document.write("--a = " + (--a) + "<br>");
+document.write("--a - --b = " + (--a - --b) + "<br>");
+document.write("--a - --b  + ++b = " + (--a - --b + ++b) + "<br>");
+document.write("--a - --b  + ++b = " + (--a - --b + ++b) + "<br>");
+document.write("--a - --b  + ++b + b-- = " + (--a - --b + ++b +b--) + "<br>" + "<br>");
+
+document.write("a is " + a + "<br>");
+document.write("b is " + b + "<br>");
+document.write(result);
+
+// Chapter # 6-9 Task 3
+
+var name = prompt("Enter your name please")
+alert("Hello user!")
+
+// Chapter # 6-9 Task 4
+
+var number = +prompt("Enter any number")
+
+
+
+
+
+
+
+
+
+
+
+
+
